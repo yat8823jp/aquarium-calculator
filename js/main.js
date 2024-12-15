9af33a0397608d9689831c2d500cd878db3b0316
y_kawanishi <yat@yat-net.com>
2024-12-15 23:29:04 +0900

    add-img

--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
 const app       = document.getElementById( 'app' );
 const makeWater = document.getElementById( 'makewater' );
 const reset     = document.querySelector( '.reset' );
+const items     = document.getElementById( 'items' );
 
 const resultsBoxies = {
 	"chalk": document.getElementById( 'result-chalk' ),
 	"zicra-benissim": document.getElementById( 'result-zicra-benissim' ),
 	"tetra-co2plus": document.getElementById( 'result-tetra-co2plus' ),
 	"water-supple": document.getElementById( 'result-water-supple' ),
-	"green-f-cold-liquid": document.getElementById( 'result-green-f-cold-liquid' ),
+	"green-f-gold-liquid": document.getElementById( 'result-green-f-gold-liquid' ),
+	"eheim-4-in-1": document.getElementById( 'result-eheim-4-in-1' ),
 }
 
 makeWater.addEventListener( 'input', () => {
@@ -22,13 +24,21 @@ reset.addEventListener( 'click', () => {
 	}
 } );
 
+
+
 function objectToArray( object ) {
 	const results = Object.entries( object );
 	return results;
 }
 
+init();
+
+function init () {
+	calcDisplay( "", "ture" );
+}
+
 //load JSON to Display.
-function calcDisplay( inputWater ) {
+function calcDisplay( inputWater, first = false ) {
 	fetch( 'data.json' )
 		.then( ( response ) => {
 			if ( ! response.ok ) {
@@ -37,13 +47,49 @@ function calcDisplay( inputWater ) {
 			return response.json()
 		} )
 		.then( ( response ) => {
-			calculator( response, inputWater );
+			const datas = objectToArray( response );
+			if ( first ) {
+				addImgTag( datas );
+			}
+			calculator( datas, inputWater );
 		} );
 }
 
+function addImgTag( datas ) {
+	const dts = items.querySelectorAll( 'dt' );
+	dts.forEach( ( dt, index ) => {
+		if( datas[index][1].img ) {
+			const imgElemtns = document.createElement('img');
+			imgElemtns.src   = datas[index][1].img;
+			imgElemtns.alt   = datas[index][0];
+
+			if( datas[index][1].affiliate ) {
+				const link = document.createElement('a');
+				link.href  = datas[index][1].affiliate;
+				link.target = "_blank";
+				link.appendChild( imgElemtns );
+				dt.prepend( link );
+			} else {
+				dt.prepend( imgElemtns );
+			}
+		}
+	} );
+}
+
+function addLink( datas ) {
+	const dts = items.querySelectorAll( 'dt' );
+	dts.forEach( ( dt, index ) => {
+		if( datas[index][1].affiliate ) {
+			const link = document.createElement('a');
+			link.href  = datas[index][1].affiliate;
+			link.target = "_blank";
+			dt.prepend( link );
+		}
+	} );
+}
+
 // JSON data to calculator.
-function calculator( response, inputWater ) {
-	const datas = objectToArray( response );
+function calculator( datas, inputWater ) {
 	const inputs = objectToArray( resultsBoxies );
 	datas.forEach( ( data, index ) => {
 		inputs[index][1].textContent = Math.ceil( ( inputWater * 1000 ) * data[1].medicine / data[1].water * 10 ) / 10;
